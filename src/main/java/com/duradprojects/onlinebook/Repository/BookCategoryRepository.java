5b88790d545c5f91bee619d398d73250d54f0c86
dikkowright <53313100+dikkowright@users.noreply.github.com>
2020-05-07 10:47:15 +0100

    book store feature 2,3

--- src/main/java/com/duradprojects/onlinebook/Repository/BookCategoryRepository.java
+++ src/main/java/com/duradprojects/onlinebook/Repository/BookCategoryRepository.java
@@ -3,6 +3,8 @@ package com.duradprojects.onlinebook.Repository;
 import com.duradprojects.onlinebook.Model.BookCategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 @RepositoryRestResource(collectionResourceRel = "bookCategory", path = "book-category")
 public interface BookCategoryRepository extends JpaRepository<BookCategoryEntity, Long> {
--- src/main/java/com/duradprojects/onlinebook/Repository/BookRepository.java
+++ src/main/java/com/duradprojects/onlinebook/Repository/BookRepository.java
@@ -1,13 +1,26 @@
 package com.duradprojects.onlinebook.Repository;
 
 import com.duradprojects.onlinebook.Model.BookEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin("http://localhost:4200")
+import javax.annotation.Resource;
+
+
 @RepositoryRestResource(collectionResourceRel = "books", path = "book")
 public interface BookRepository extends JpaRepository<BookEntity, Long> {
+
+    @RestResource(path = "categoryid" )
+   Page<BookEntity> findByCategoryId(@Param("id") Long id, Pageable pageable);
+
+    @RestResource(path = "searchbykeyword" )
+    Page<BookEntity> findByNameContaining(@Param("name")  String keyword, Pageable pageable);
+
 
 
 
